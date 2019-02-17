88b7888e3d197d3b2c907da06b70bf61ac9bce14
Ben Puderer <bpuderer@yahoo.com>
2019-02-17 14:59:40 -0500

    requests.codes

--- thirdparty/requests_qs.py
+++ thirdparty/requests_qs.py
@@ -7,18 +7,22 @@ _TIMEOUT = 0.3
 # GET
 resp = requests.get('http://127.0.0.1:5000/hello', timeout=_TIMEOUT)
 print(resp.status_code, resp.text)
+# http://docs.python-requests.org/en/master/api/#requests.codes
+# vs. http.HTTPStatus.OK https://docs.python.org/3/library/http.html
+assert resp.status_code == requests.codes.ok
 
 
 # GET
 resp = requests.get('http://127.0.0.1:5000/users/2010', timeout=_TIMEOUT)
 print(resp.status_code, resp.json())
+assert resp.status_code == requests.codes.ok
 
 
 # POST
 json_data = {'_id': 2010, 'name': 'Cleese'}
 resp = requests.post('http://127.0.0.1:5000/users/2010', json=json_data, timeout=_TIMEOUT)
 print(resp.status_code, resp.json())
-
+assert resp.status_code == requests.codes.created
 
 # POST
 headers = {'test_header': '123'}
@@ -27,3 +31,4 @@ data = 'some data'
 resp = requests.post('http://127.0.0.1:5000/users/2010', params=params,
                      data=data, headers=headers, timeout=_TIMEOUT)
 print(resp.status_code, resp.text)
+assert resp.status_code == requests.codes.bad_request
