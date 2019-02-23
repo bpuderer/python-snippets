553a3e7e1e0aa880cba1050e63cd50c86d4560a4
Ben Puderer <bpuderer@yahoo.com>
2019-02-23 12:03:45 -0500

    json yaml util. serialization ftn

--- stdlib/json_qs.py
+++ stdlib/json_qs.py
@@ -1,25 +1,27 @@
 import json
-
+from datetime import datetime
 
 # dumps - serialization - object to string
 # loads - deserialization - string to object
 
-obj = {'_id': 0, 'things': [{'bool': True, 'float': 3.14, 'none': None}], 'tuple': (1, 2), 'str': 'str val'}
-obj_str = json.dumps(obj)
+obj = {'_id': 0, 'things': [{'bool': True, 'float': 3.14, 'none': None}], 'tuple': (1, 2), 'str': 'str val', 'timestamp': datetime.now()}
+# https://docs.python.org/3/library/json.html#json.dump
+# default should be a function that gets called for objects that can't otherwise be serialized
+obj_str = json.dumps(obj, default=str)
 
 print("object:", obj)
 print("\nserialized:", obj_str)
 print("\ndeserialized (note tuple):", json.loads(obj_str))
 
 # in py 3.4, if indent != None, separators=(',', ': ')
-print("\nformatted:", json.dumps(obj, indent=2, sort_keys=True))
+print("\nformatted:", json.dumps(obj, indent=2, sort_keys=True, default=str))
 
 
 test_file = 'test.json'
 
 with open(test_file, 'w') as f:
     print("serialized to", test_file)
-    json.dump(obj, f)
+    json.dump(obj, f, default=str)
 
 with open(test_file) as f:
     print("deserialized from", test_file, json.load(f))
--- util/utility.py
+++ util/utility.py
@@ -3,6 +3,8 @@ import json
 import sys
 from xml.dom import minidom
 
+import yaml
+
 
 if len(sys.argv) == 1 or sys.argv[1] == '-':
     s = sys.stdin.read()
@@ -20,6 +22,12 @@ else:
 #out = base64.b64encode(s.encode()).decode()
 
 # base64 decode
-out = base64.b64decode(s.encode()).decode()
+#out = base64.b64decode(s.encode()).decode()
+
+# json to yaml
+out = yaml.dump(json.loads(s))
+
+# yaml to json
+#out = json.dumps(yaml.load(s), indent=2, default=str)
 
 print(out)
