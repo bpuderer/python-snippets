bea8b9c904587d1a6320d0157d46c2e7a9d185a3
Ben Puderer <bpuderer@yahoo.com>
2020-05-06 11:21:58 -0400

    pyqt5 intro

---
+++ thirdparty/pyqt5_qs/hello-world.py
@@ -0,0 +1,40 @@
+# Example code from Bo Milanovich's "Python Desktop Application Development"
+# course on LinkedIn Learning
+# Completed 5/6/20
+# Qt pronounced "cute"
+from PyQt5.QtWidgets import *
+import sys
+
+# https://doc.qt.io/qt-5/qtwidgets-module.html
+
+class HelloWorld(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        # super(HelloWorld, self).__init__()
+
+        # QVBoxLayout, QHBoxLayout, QGridLayout, QFormLayout
+        layout = QGridLayout()
+
+        self.label = QLabel("<b>Hello World!</b>")
+        line_edit = QLineEdit()
+        button = QPushButton("Close")
+
+        layout.addWidget(self.label, 0, 0)  # row, column
+        layout.addWidget(line_edit, 0, 1)
+        layout.addWidget(button, 1, 1)
+
+        self.setLayout(layout)
+
+        # events/signals <---> handlers/slots
+        button.clicked.connect(self.close)
+        line_edit.textChanged.connect(self.change_text_label)
+
+
+    def change_text_label(self, text):
+        self.label.setText("Hello " + text + "!")
+
+
+app = QApplication(sys.argv)
+dialog = HelloWorld()
+dialog.show()
+sys.exit(app.exec_())
